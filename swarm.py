d9c01fcac28d31179aec58d2d14e06c4c0f0171d
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2013-02-24 15:32:01 +0100

    Important clarification of the code

--- swarm.py
+++ swarm.py
@@ -8,26 +8,26 @@
 from __future__ import print_function
 
 __author__ = "Frédéric Mahé <mahe@rhrk.uni-kl.fr>"
-__date__ = "2013/01/01"
-__version__ = "$Revision: 3.0"
+__date__ = "2013/02/24"
+__version__ = "$Revision: 3.1"
 
 import sys
 from Bio import SeqIO
 from Bio.Seq import Seq
 from optparse import OptionParser
 
-#**********************************************************************#
-#                                                                      #
-#                            Functions                                 #
-#                                                                      #
-#**********************************************************************#
+#******************************************************************************#
+#                                                                              #
+#                                  Functions                                   #
+#                                                                              #
+#******************************************************************************#
 
 def option_parse():
     """
     Parse arguments from command line.
     """
-    desc = """Find clusters of nearly identical sequences in giant
-    barcoding projects."""
+    desc = """Find clusters of nearly identical amplicons in giant
+    amplicon-based environmental or clinical projects."""
     
     parser = OptionParser(usage="usage: %prog --input_file filename --threshold integer",
         description = desc,
@@ -37,7 +37,7 @@ def option_parse():
         metavar = "<FILENAME>",
         action = "store",
         dest = "input_file",
-        help = "set <FILENAME> as input file. Ungapped lowercase fasta file (acgt only).")
+        help = "set <FILENAME> as input file. Ungapped fasta file (acgt only, case insensitive).")
 
     parser.add_option("-t", "--threshold",
         metavar = "<THRESHOLD>",
@@ -83,7 +83,7 @@ def needleman_wunsch(seqA, seqB):
     #
     # Replace S mapping with a simple if statement (faster)? If the
     # matrix is symetrical (delete == insert penalty), there is no
-    # need to compute both? In think I am wrong.
+    # need to compute both? I think I am wrong.
     for i in I[1:]:
 	for j in J[1:]:
             match = F[i-1][j-1] + S[A[i-1]][B[j-1]]
@@ -96,11 +96,33 @@ def needleman_wunsch(seqA, seqB):
     return score
 
 
-#**********************************************************************#
-#                                                                      #
-#                              Body                                    #
-#                                                                      #
-#**********************************************************************#
+def find_kseeds(candidates, status, frontier, amplicons, max_length_difference,
+                max_number_of_mismatches, threshold):
+    """
+    For a given subseed, find all k-seeds with threshold or less
+    differences with the subseed. Candidates already have been
+    filtered for "left hand" reads. Do not treat already assigned
+    amplicons. Do not compare amplicons we know to be too distant from
+    the seed. Do not compare amplicons with a length difference
+    greater than the threshold. Do not compare amplicons if their
+    nucleotide profiles are too divergent.
+    
+    Note: the wrapping is not correct here (line too long), but do not
+    know how to correct it.
+    """
+    hits = [j for j, d in candidates
+            if status[j] and
+            d <= frontier and
+            abs(cmp(amplicons[l][2], amplicons[j][2])) <= max_length_difference and
+            sum([abs(cmp(couple[0], couple[1])) for couple in zip(amplicons[l][4], amplicons[j][4])]) <= 2 * max_number_of_mismatches - abs(cmp(amplicons[l][2], amplicons[j][2])) and
+            needleman_wunsch(amplicons[l][3], amplicons[j][3]) <= threshold]
+    return hits
+
+#******************************************************************************#
+#                                                                              #
+#                                     Body                                     #
+#                                                                              #
+#******************************************************************************#
 
 if __name__ == '__main__':
     
@@ -120,7 +142,6 @@ if __name__ == '__main__':
     # elif 13 <= threshold < 16: max_length_difference = 3
     # elif 16 <= threshold < 19: max_length_difference = 4
     # elif 19 <= threshold < 21: max_length_difference = 5
-    # else: max_length_difference = 100
     # That can be expressed like that:
     if threshold < d:
         max_length_difference = 0
@@ -128,7 +149,7 @@ if __name__ == '__main__':
         max_length_difference = ((threshold - d) / p) + 1
 
     # Scoring system and max number of mismatches
-    # 0: identical sequences
+    # 0: identical amplicons
     # 3: 1 mismatch
     # 6: 2 mismatches
     # 7: 1 gap of length 1
@@ -141,51 +162,49 @@ if __name__ == '__main__':
     # 16: 1 gap of length 4, or 1 gap of length 1 + 3 mismatches, or 1 gap of length 2 + 2 mismatches, or 1 gap of length 3 + 1 mismatch 
     max_number_of_mismatches = threshold / p
 
-    # Build a list of sequences and count nucleotide occurences
+    # Build a list of amplicons and count nucleotide occurences
     input_format = "fasta"
     nucleotides = ("a","c","g","t")
-    records_list = list()
-    status = list()
     with open(input_file, "rU") as input_file:
-        records = SeqIO.parse(input_file, input_format)
-        records_list = [(record.id.split("_")[0],
-                         record.id.split("_")[1],
-                         len(record.seq),
-                         str(record.seq),
-                         [record.seq.lower().count(n) for n in nucleotides])
-                        for record in records]
-        status = [True] * len(records_list)
+        amplicons = [(record.id.split("_")[0],
+                    record.id.split("_")[1],
+                    len(record.seq),
+                    str(record.seq),
+                    [record.seq.lower().count(n) for n in nucleotides])
+                   for record in SeqIO.parse(input_file, input_format)]
+        status = [True] * len(amplicons)
         
     # Start swarming
     while True:
         try:
-            # Search the next master seed
+            # Search the next master seed (first amplicon with status
+            # set to "True")
             i = status.index(True)
+            swarm = [amplicons[i][0]]
+            status[i] = False
         except ValueError:
-            # All sequences have been treated
+            # All amplicons have been treated, stop the process
             break
 
-        # Master seed
-        swarm = [records_list[i][0]]
-        status[i] = False
-
-        # List remaining non-swarmed sequences (boolean test)
-        comparisons = [j for j, state in enumerate(status) if state]
+        # List remaining non-swarmed amplicons (status set to True)
+        comparisons = [j for j, status[j] in enumerate(status) if status[j]]
         
-        # Deal with the last remaining item
+        # No amplicons remain, stop the process
         if not comparisons:
             print(" ".join(swarm), file=sys.stdout)
             break
 
-        # Candidates list is initialized for each major seed
-        candidates = [(j, needleman_wunsch(records_list[i][3], records_list[j][3])) for j in comparisons]
+        # Compute and store pairwise distances between the master seed
+        # and all remaining amplicons ("candidates")
+        candidates = [(j, needleman_wunsch(amplicons[i][3], amplicons[j][3]))
+                      for j in comparisons]
 
-        # Parse candidates and select sons
+        # Parse candidates and select the first generation of subseeds
         firstseeds = [j for j, d in candidates if d <= threshold]
-        swarm.extend([records_list[j][0] for j in firstseeds])
+        swarm.extend([amplicons[j][0] for j in firstseeds])
         for j in firstseeds: status[j] = False
 
-        # Loop other the first subseeds (if they exists)
+        # Loop other the first subseeds (if they exist)
         all_subseeds = list()
         if firstseeds:
             all_subseeds.append(firstseeds)
@@ -193,27 +212,13 @@ if __name__ == '__main__':
             for k, subseeds in enumerate(all_subseeds):
                 nextseeds = list()
                 frontier = (k + 2) * threshold
-                # I could replace that for loop with a list
-                # comprehension, but the risk is to perform un-needed
-                # amplicon comparisons, as the status list is not
-                # updated regularly.
                 for l in subseeds:
-                    # Candidates already have been filtered for "left
-                    # hand" reads. Do not treat already assigned
-                    # sequences. Do not compare sequences we know to
-                    # be too distant from the seed. Do not compare
-                    # sequences with a length difference greater than
-                    # the threshold. Do not compare sequences if their
-                    # nucleotide profiles are too divergent.
-                    hits = [j for j, d in candidates
-                            if status[j]
-                            and d <= frontier
-                            and abs(cmp(records_list[l][2], records_list[j][2])) <= max_length_difference
-                            and sum([abs(cmp(couple[0], couple[1])) for couple in zip(records_list[l][4], records_list[j][4])]) <= 2 * max_number_of_mismatches - abs(cmp(records_list[l][2], records_list[j][2]))
-                            and needleman_wunsch(records_list[l][3], records_list[j][3]) <= threshold]
+                    hits = find_kseeds(candidates, status, frontier,
+                                       amplicons, max_length_difference,
+                                       max_number_of_mismatches, threshold)
                     if hits:
                         nextseeds.extend(hits)
-                        swarm.extend([records_list[j][0] for j in hits])
+                        swarm.extend([amplicons[j][0] for j in hits])
                         for j in hits: status[j] = False
                 # Stop condition
                 if nextseeds:
@@ -223,7 +228,7 @@ if __name__ == '__main__':
                     print(" ".join(swarm), file=sys.stdout)
                     break
         else:
-            # Deal with isolated sequences
+            # Deal with isolated amplicons
             print(" ".join(swarm), file=sys.stdout)
         
 sys.exit(0)
@@ -231,23 +236,16 @@ sys.exit(0)
 # Profiling
 # ---------
 #
-# python -m cProfile swarm.py -i file.fas > tmp
+# python -m cProfile swarm.py -i ../examples/AF091148.fas > tmp
 #
 # Expected results
 # ----------------
 #
-# time python swarm.py -i AF091148.fas | awk '{print NF}' | sort -nr | uniq -c
-#       1 878
-#       1 423
-#       1 15
-#       1 5
-#       1 4
-#       4 3
-#       5 2
-#      56 1
+# time python swarm.py -i ../examples/AF091148.fas | awk '{print NF}' | sort -nr | uniq -c
+#  1 1310
+#  1 31
+#  1 15
+#  1 4
+#  3 2
+# 37 1
 #
-#
-# To Do:
-#
-# - Refactoring and simplification of the code,
-# - write the needleman-wunsch function with numpy(?)
